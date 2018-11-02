d216ac4ff736223f422869a92911b083d7b7f290
Andreas Garnaes <andreas.garnaes@gmail.com>
2018-11-02 15:16:49 +0100

    First stab at websockets

--- graphql-cohttp/src/graphql_cohttp.ml
+++ graphql-cohttp/src/graphql_cohttp.ml
@@ -8,20 +8,29 @@ end
 
 module Make
   (Schema : Graphql_intf.Schema)
+  (Io : Cohttp.S.IO with type 'a t = 'a Schema.Io.t)
   (Body : HttpBody with type +'a io := 'a Schema.Io.t) = struct
 
-  module Io = Schema.Io
+  module Ws = Websocket.Connection.Make (Io)
+  module Websocket_transport = Websocket_handler.Make (Schema.Io) (Ws)
 
-  let (>>=) = Io.bind
+  let (>>=) = Io.(>>=)
+
+  type response_action =
+    [ `Expert of Cohttp.Response.t
+                 * (Io.ic
+                    -> Io.oc
+                    -> unit Io.t)
+    | `Response of Cohttp.Response.t * Body.t ]
 
   type 'conn callback =
     'conn ->
     Cohttp.Request.t ->
     Body.t ->
-    (Cohttp.Response.t * Body.t) Schema.Io.t
+    response_action Io.t
 
   let respond_string ~status ~body () =
-    Io.return (Cohttp.Response.make ~status (), Body.of_string body)
+    Io.return (`Response (Cohttp.Response.make ~status (), Body.of_string body))
 
   let static_file_response path =
     match Assets.read path with
@@ -57,11 +66,16 @@ module Make
       let body = Yojson.Basic.to_string err in
       respond_string ~status:`Internal_server_error ~body ()
 
-  let make_callback make_context schema _conn (req : Cohttp.Request.t) body =
+  let make_callback : (Cohttp.Request.t -> 'ctx) -> 'ctx Schema.schema -> 'conn callback = fun make_context schema _conn (req : Cohttp.Request.t) body ->
     let req_path = Cohttp.Request.uri req |> Uri.path in
     let path_parts = Str.(split (regexp "/") req_path) in
     match req.meth, path_parts with
-    | `GET,  ["graphql"]       -> static_file_response "index.html"
+    | `GET,  ["graphql"] ->
+      if Cohttp.Header.get req.Cohttp.Request.headers "Connection" = Some "Upgrade" && Cohttp.Header.get req.headers "Upgrade" = Some "websocket" then
+        let handle_conn =  Websocket_transport.handle (execute_query (make_context req) schema) in
+        Io.return (Ws.upgrade_connection req handle_conn)
+      else
+        static_file_response "index.html"
     | `GET,  ["graphql"; path] -> static_file_response path
     | `POST, ["graphql"]       -> execute_request schema (make_context req) req body
     | _ -> respond_string ~status:`Not_found ~body:"" ()
--- graphql-cohttp/src/graphql_cohttp.mli
+++ graphql-cohttp/src/graphql_cohttp.mli
@@ -8,20 +8,30 @@ end
 
 module Make
   (Schema : Graphql_intf.Schema)
+  (Io : Cohttp.S.IO with type 'a t = 'a Schema.Io.t)
   (Body : HttpBody with type +'a io := 'a Schema.Io.t) : sig
+
+  type response_action =
+    (* The connection is not closed in the [`Expert] case until the [unit
+       Async_kernel.Deferred.t] becomes determined. *)
+    [ `Expert of Cohttp.Response.t
+                 * (Io.ic
+                    -> Io.oc
+                    -> unit Io.t)
+    | `Response of Cohttp.Response.t * Body.t ]
 
   type 'conn callback =
     'conn ->
     Cohttp.Request.t ->
     Body.t ->
-    (Cohttp.Response.t * Body.t) Schema.Io.t
+    response_action Schema.Io.t
 
   val execute_request :
     'ctx Schema.schema ->
     'ctx ->
     Cohttp.Request.t ->
     Body.t ->
-    (Cohttp.Response.t * Body.t) Schema.Io.t
+    response_action Schema.Io.t
 
   val make_callback :
     (Cohttp.Request.t -> 'ctx) ->
